cc0d1d45f8caf8cb7de6c3c5bbcc6d1400507e22
ahmedhussein74 <ahmedhusseinofficial22@gmail.com>
2023-04-30 11:08:16 +0200

    commit

---
+++ models/userModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 8,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    phone: {
+      type: String,
+      trim: true,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+    cardNumber: {
+      type: String,
+    },
+    history: { type: Array, default: [] },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
